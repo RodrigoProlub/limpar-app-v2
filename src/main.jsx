a9b5b94dac983a9e20bd630d213ed0bb876fde18
RodrigoProlub <rodrigo.prolub@gmail.com>
2026-06-20 00:11:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ClienteProvider } from './ClienteContext.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClienteProvider>
+      <App />
+    </ClienteProvider>
   </React.StrictMode>
 )
